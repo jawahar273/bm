066d4f2f3d0e04d7cf4422aeb44d78d303c488d3
jawahar273 <jawahar273@gmail.com>
2018-01-23 15:41:37 +0530

    change in settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -44,7 +44,7 @@ CACHES = {
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INSTALLED_APPS += ['debug_toolbar', 'fixmydjango']
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', 'jawahar.pythonanywhere.com' ]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
