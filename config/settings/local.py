aaac76c28edbca8da0cef3a66c332adc14864ec0
jawahar273 <jawahar273@gmail.com>
2018-04-02 23:30:50 +0530

    updates

--- config/settings/local.py
+++ config/settings/local.py
@@ -44,7 +44,7 @@ CACHES = {
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INSTALLED_APPS += ['debug_toolbar', 'rest_framework_swagger',]
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', 'jawahar.pythonanywhere.com' ]
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', '0.0.0.0' ]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
