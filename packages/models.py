9fdd504c8f4da5ec98dd4dfd09252e6cc4f84f91
jawahar273 <jawahar273@gmail.com>
2018-04-29 20:53:20 +0530

    fixing the bug

--- packages/models.py
+++ packages/models.py
@@ -141,7 +141,7 @@ def validate_max_time_interval(value):
     '''This method is used as validator
     to check range of times.
     '''
-    if value < Geoloc.min_interval_time and value > Geoloc.max_interval_time:
+    if value < Geoloc.min_interval_time() and value > Geoloc.max_interval_time():
         return ValidationError('Can not Exceed the max or'
                                'min of interval of 10 mins 8 hours'
                                'repecatively' % (Geoloc.max_interval_time,
