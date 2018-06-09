1ba1dd4d860001a8e1cc83f26703362b62d82623
jawahar273 <jawahar273@gmail.com>
2018-06-09 09:46:44 +0530

    adding of testing in util packages

--- packages/tests/test_utils.py
+++ packages/tests/test_utils.py
@@ -1,7 +1,12 @@
 
 from test_plus.test import TestCase
 
-from packages.utils import to_hexdigit, to_percentage, validate_less_than_today
+from packages.utils import (
+    to_hexdigit,
+    to_percentage,
+    validate_less_than_today,
+    find_dict_value,
+)
 
 
 # Create your tests here.
@@ -24,3 +29,9 @@ class TestPackagesUtils(TestCase):
         self.assertTrue(value)
         value = validate_less_than_today("2020-05-01")
         self.assertFalse(vale)
+
+    def test_find_dict_value(self):
+        testing = {"test-1": 1, "test-2": 2, "test-3": 3}
+        value = find_dict_value(3, testing)
+
+        self.assertEqual(value, "test-3")
